da6595f1209f4b79817e94feea5a3bc57f3e3a4e
Dhruv-Raichand <dhruvraichand70@gmail.com>
2025-08-24 19:39:26 +0530

    Refactor application structure: integrate Redux store, update routing, and create new components for Feed, Profile, and Connections

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { BASE_URL } from "../utils/constants";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      const req = await axios.post(
-        "http://localhost:3000/login",
+      const res = await axios.post(
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
+      console.log(res.data);
+      dispatch(addUser(res.data));
+      navigate("/");
     } catch (err) {
       console.log(err);
     }
