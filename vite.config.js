c2a1287c492f0dc524a9a7e4b179878c694a6d61
Dhruv-Raichand <dhruvraichand70@gmail.com>
2025-12-15 17:35:54 +0530

    refactor: update visualizer configuration in vite.config.js for conditional analysis

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
+    process.env.ANALYZE &&
+      visualizer({
+        open: false, // IMPORTANT for EC2
+        gzipSize: true,
+        brotliSize: true,
+      }),
+  ].filter(Boolean),
 });
