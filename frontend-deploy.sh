07624374e1cf13ae19db24f6db2bf65c8af0df2c
Ubuntu <ubuntu@ip-172-31-22-153.eu-north-1.compute.internal>
2026-04-20 05:08:41 +0000

    Add frontend deploy script

---
+++ frontend-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Load nvm
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+
+# Use your node version
+nvm use node
+
+cd ~/devlink-frontend
+
+echo "Pulling latest code..."
+git pull origin main
+
+echo "Installing dependencies..."
+npm install
+
+echo "Building project..."
+npm run build
+
+echo "Deploying to Nginx..."
+rm -rf /var/www/html/*
+cp -r dist/* /var/www/html
+
+echo "Frontend deployed!"
